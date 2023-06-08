617fac52e34c25aca30d503b747d08f486092fea
LoZioo <60042676+LoZioo@users.noreply.github.com>
2023-06-08 13:36:21 +0200

    docs

--- config.sh
+++ config.sh
@@ -6,6 +6,6 @@ USER="ubuntu"
 # ds-net-1
 SERVER_BC_ADDRESS="90.147.185.8"
 
-# Docker settings.
+# Docker buildx settings.
 BUILD_PLATFORMS="linux/amd64"
 DOCKERHUB_IMAGE="lozioo/blockchain:latest"
